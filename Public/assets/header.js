7712498c99c95ba8ed8f9f1d7b324f6a99fa34e7
Boris Karakolev <boriskarakolev@hotmail.com>
2020-05-02 22:49:08 +0200

    ''

--- Public/assets/header.js
+++ Public/assets/header.js
@@ -1,25 +1,39 @@
-window.onscroll = function() {stickyN()};
-let header = document.getElementById('sticky');
-let sticky = header.offsetTop;
+// window.onscroll = function() {stickyN()};
+// let sticky = header.offsetTop;
 
-let stickyN = () => {
+// let stickyN = () => {
 
-    if (window.pageYOffset > sticky) {
-        header.classList.add('sticky');
-        header.style.transition = 'all 0.3s ease 0s';
-        header.style.backgroundColor = '#fafaf0';
-        header.style.opacity = 0.8;
-        document.querySelector('.login').style.borderColor = '#5cb85c';
-        document.querySelector('.login').style.color = '#5cb85c';
+//     if (scrollPosition > 100) {
+//         header.classList.add('sticky');
+//         header.style.transition = 'all 0.3s ease 0s';
+//         header.style.backgroundColor = '#fafaf0';
+//         header.style.opacity = 0.8;
+//         document.querySelector('.login').style.borderColor = '#639ecf';
+//         document.querySelector('.login').style.color = '#639ecf';
 
-    } else if (window.pageYOffset == sticky) {
-        header.classList.remove('sticky');
-        header.style.background = 'unset';
-        header.style.transition = 'all 0.4s ease 0s';
-        header.style.opacity = 1;
-        document.querySelector('.login').style.borderColor = 'white';
-        document.querySelector('.login').style.color = 'white';
+//     } else {
+//         header.classList.remove('sticky');
+//         header.style.background = 'unset';
+//         header.style.transition = 'all 0.4s ease 0s';
+//         header.style.opacity = 1;
+//         document.querySelector('.login').style.borderColor = 'white';
+//         document.querySelector('.login').style.color = 'white';
+//     }
+// }
+
+// window.scroll(function() {stickyN});
+
+let checkHeader = _.throttle (() => {
+
+    let scrollPosition = Math.round(window.scrollY);
+
+    if (scrollPosition > 100) {
+        document.getElementById('sticky').classList.add('sticky');
     }
-}
+    else 
+    {
+        document.getElementById('sticky').classList.remove('sticky');
+    }
+}, 300);
 
-window.scroll(function() {stickyN});
+window.addEventListener('scroll', checkHeader);
