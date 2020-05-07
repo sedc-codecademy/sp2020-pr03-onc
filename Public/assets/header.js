16a22eff6a4c3bedbea0ffa88b0fb8360498ec39
Boris Karakolev <boriskarakolev@hotmail.com>
2020-05-07 14:16:20 +0200

    ''

--- Public/assets/header.js
+++ Public/assets/header.js
@@ -1,39 +1,21 @@
-// window.onscroll = function() {stickyN()};
-// let sticky = header.offsetTop;
+window.addEventListener('scroll', function () {
+    let logoImg = document.querySelector('.logo h1 a');
+    let header = document.querySelector('#head');
+    let login = document.querySelector('#logB');
 
-// let stickyN = () => {
-
-//     if (scrollPosition > 100) {
-//         header.classList.add('sticky');
-//         header.style.transition = 'all 0.3s ease 0s';
-//         header.style.backgroundColor = '#fafaf0';
-//         header.style.opacity = 0.8;
-//         document.querySelector('.login').style.borderColor = '#639ecf';
-//         document.querySelector('.login').style.color = '#639ecf';
-
-//     } else {
-//         header.classList.remove('sticky');
-//         header.style.background = 'unset';
-//         header.style.transition = 'all 0.4s ease 0s';
-//         header.style.opacity = 1;
-//         document.querySelector('.login').style.borderColor = 'white';
-//         document.querySelector('.login').style.color = 'white';
-//     }
-// }
-
-// window.scroll(function() {stickyN});
-
-let checkHeader = _.throttle (() => {
-
-    let scrollPosition = Math.round(window.scrollY);
-
-    if (scrollPosition > 100) {
-        document.getElementById('sticky').classList.add('sticky');
+    if (window.pageYOffset > 10) {
+        logoImg.innerHTML = `<h2>Mindify</h2>`;
+        header.classList.add('headerHe');
+        login.classList.add('loginScroll');
     }
     else 
     {
-        document.getElementById('sticky').classList.remove('sticky');
+        logoImg.innerHTML = `<h1>Mindify</h1>`;
+        header.classList.remove('headerHe');
+        login.classList.remove('loginScroll');
     }
-}, 300);
 
-window.addEventListener('scroll', checkHeader);
+
+
+
+})
